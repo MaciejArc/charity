4ecbde80afe516c53dddb5ab11822db32099ce9d
MaciejNowak2021 <maciejnowak1994@gmail.com>
2021-10-07 21:58:32 +0200

    SB Admin, header footer

--- src/main/java/pl/coderslab/charity/controller/AdminController.java
+++ src/main/java/pl/coderslab/charity/controller/AdminController.java
@@ -2,12 +2,22 @@ package pl.coderslab.charity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping("/admin")
+
+    @GetMapping("")
     public String admin(){
         return "admin/index";
     }
+
+    @GetMapping("/institution")
+    public String istitution(){
+
+        return "admin/institution";
+    }
 }
+
---
+++ src/main/webapp/WEB-INF/views/admin/institution.jsp
@@ -0,0 +1,15 @@
+<%@ include file="header.jsp" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<div class="container-fluid">
+
+
+
+
+
+
+</div>
+</div>
+
+
+<%@ include file="footer.jsp" %>
